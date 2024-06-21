47f30d01012c92d93e43423211d817c5f1344bea
Purdea Bogdan <bogdanpurdea14@gmail.com>
2024-06-21 20:53:09 +0300

    Obtain env variables

--- server/config/index.js
+++ server/config/index.js
@@ -1,11 +1,16 @@
-var configValues = require('./config');
+const env = process.env.ENVIRONMENT;
+if(env !== "Production")
+    var configValues = require('./config');
 
 module.exports = {
     //Ideally store encripted credentials and decript them here before using them
 
     getDbConnectionString: function() {
-        return 'mongodb+srv://' + configValues.username + ':' + 
-            configValues.password + configValues.database;
+        if(env !== "Production")
+            return 'mongodb+srv://' + configValues.username + ':' + 
+                configValues.password + configValues.database;
+        return 'mongodb+srv://' + process.env.DBUSERNAME + ':' + 
+            process.env.DBPASSWORD + process.env.DB;
     }
 
 }
